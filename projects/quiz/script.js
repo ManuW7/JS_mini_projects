f9829df492ea8e17099d2ed03369644158181ca1
Gleb <409075@niuitmo.ru>
2025-09-14 15:24:14 +0300

    initiale starting page

--- projects/quiz/script.js
+++ projects/quiz/script.js
@@ -2,19 +2,19 @@ const starting_page_html = `<main>
         <h1>Brain Quiz</h1>
         <p class="annotation">Test your knowlege!</p>
         <div class="themes_container">
-          <button class="theme">
+          <button class="theme" name='All'>
             <div class="theme_icon">🌍</div>
             <h3 class="theme_title">Все категории</h3>
           </button>
-          <button class="theme">
+          <button class="theme" name='IT'>
             <div class="theme_icon">💻</div>
             <h3 class="theme_title">IT и технологии</h3>
           </button>
-          <button class="theme">
+          <button class="theme" name='Science'>
             <div class="theme_icon">🔬</div>
             <h3 class="theme_title">Наука</h3>
           </button>
-          <button class="theme">
+          <button class="theme" name='History'>
             <div class="theme_icon">📜</div>
             <h3 class="theme_title">История</h3>
           </button>
@@ -58,6 +58,45 @@ const final_page_html = `<main>
         </div>
       </main>`;
 
-// document.querySelector(".app").innerHTML = starting_page_html;
+const app_interface = document.querySelector(".app");
 
-// document.querySelector(".app").innerHTML = question_page_html;
+//Добавляем изначальный экран
+app_interface.innerHTML = starting_page_html;
+
+const theme_choice_div = document.querySelector(".themes_container");
+const timer_checkbox = document.querySelector(".checkbox");
+
+// Переменная для сохранения выбранной темы
+let theme = null;
+
+theme_choice_div.addEventListener("click", function (event) {
+  const clickedTheme = event.target.closest("button.theme");
+  if (!clickedTheme) return;
+
+  // Если это уже выделенная тема - просто снимаем выделение
+  if (theme === clickedTheme) {
+    clickedTheme.classList.remove("pressed");
+    theme = null;
+    return;
+  }
+
+  // Снимаем выделение с предыдущей темы
+  if (theme) {
+    theme.classList.remove("pressed");
+  }
+
+  // Выделяем новую тему
+  clickedTheme.classList.add("pressed");
+  theme = clickedTheme;
+});
+
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("start_quiz_btn")) {
+    if (theme) {
+      console.log(theme.getAttribute("name"));
+      console.log(timer_checkbox.checked);
+    } else {
+      alert("Тема не выбрана. Пожалуйста, выберите тему");
+    }
+  }
+});
