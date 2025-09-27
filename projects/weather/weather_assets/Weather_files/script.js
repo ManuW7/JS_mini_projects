ea7b8707ad0a1ce2c6e1e77bc0a1fa732e608f53
Gleb <409075@niuitmo.ru>
2025-09-27 14:11:43 +0300

    delete some files

--- projects/weather/weather_assets/Weather_files/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Я тут");
-const cityFinderForm = document.querySelector(".cityFinderForm");
-
-cityFinderForm.addEventListener("submit", function (event) {
-  event.preventDefault;
-  console.log("Произошел Submit");
-});
